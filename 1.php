b1c1351e422cdc6869cfdfd9681c0fbd7602f222
unknown <2358871230@qq.com>
2020-02-26 19:33:06 +0800

    try

---
+++ 1.php
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+        <form methon="post" action="test.php">
+            <input type="myname" value="l"/>
+            <input type="submit" value="提交"/>
+        </form>
+    </body>
+</html>
