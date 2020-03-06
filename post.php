03d1f7ee6028f67998425a03637ad26b74384deb
unknown <2358871230@qq.com>
2020-03-06 11:51:35 +0800

    登录

---
+++ post.php
@@ -0,0 +1,22 @@
+<?php
+    $change = false;
+    //变量
+    $username = $_POST["Username"];
+
+    if(!$username){
+        $change = true;
+        echo"请填写用户名！<br/>";
+    }
+
+    $password = $_POST["passwoad"];
+    if(!$password){
+        $change = true;
+        echo"请填写密码！<br/>";
+    }else{
+        if(strlen($password)<6){
+            echo"密码至少需要6个字符！<br/>";
+            $change = true;
+        }
+    }
+    
+    if(!$change) echo"登陆成功！";
